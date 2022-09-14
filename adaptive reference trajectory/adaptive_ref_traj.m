4aca7a136d2806e057c5152ee79a932c86f4997a
abinashagasti <ee20s022@smail.iitm.ac.in>
2022-09-14 10:23:27 +0530

    final verifications

--- adaptive reference trajectory/adaptive_ref_traj.m
+++ adaptive reference trajectory/adaptive_ref_traj.m
@@ -3,32 +3,32 @@ clear
 clc
 close all
 
-%%
+%
 p1=1;p2=1;
 x0=[p1;p2;pi/4;0;0;0]; % Starting point
 q1=20;q2=30; % Opponent location
 v=1;ve=0.5;
 param=[q1;q2;v;ve];
-[t,x]=rk4(0.01,[0,42.5],x0,param);
+[t,x,xe,ye]=rk4(0.01,[0,6],x0,param);
 
 %%
 plot(x(1,:),x(2,:))
 hold on
-plot(q1,q2,'ro')
+plot(xe,ye)
 xlabel('x')
 ylabel('y')
 title('Actual system trajectory on xy plane')
 
-psid=atan2(q2-p2,q1-p1);
-figure
-plot(t,x(1,:)-(p1+v*cos(psid)*t))
-title('Error in x trajectory')
-% legend('x(t)','x_d(t)')
-figure
-plot(t,x(2,:)-(p2+v*sin(psid)*t))
-title('Error in y Trajectory')
-% legend('y(t)','y_d(t)')
-figure
-plot(t,x(3,:)-psid*ones(size(t)))
-title('Error in \psi Trajectory')
-% legend('\psi(t)','\psi_d(t)')
+% psid=atan2(q2-p2,q1-p1);
+% figure
+% plot(t,x(1,:)-(p1+v*cos(psid)*t))
+% title('Error in x trajectory')
+% % legend('x(t)','x_d(t)')
+% figure
+% plot(t,x(2,:)-(p2+v*sin(psid)*t))
+% title('Error in y Trajectory')
+% % legend('y(t)','y_d(t)')
+% figure
+% plot(t,x(3,:)-psid*ones(size(t)))
+% title('Error in \psi Trajectory')
+% % legend('\psi(t)','\psi_d(t)')
--- adaptive reference trajectory/rk4.m
+++ adaptive reference trajectory/rk4.m
@@ -1,4 +1,4 @@
-function [t,x] = rk4(h,T,x0,param)
+function [t,x,xe,ye] = rk4(h,T,x0,param)
 % This is a custom RK order 4 ODE solver that allows to tinker with the
 % code better.
 %     h=0.1;
@@ -6,16 +6,19 @@ function [t,x] = rk4(h,T,x0,param)
     f=@auv_dynamics_adaptive;
     x=zeros(length(x0),length(t));
     x(:,1)=x0;
-    param=[x0(1);x0(2);param(1:3)]; %Switch for point tracking
-%     q1=param(1);q2=param(2);v=param(3);ve=param(4);
-%     psid=atan2(q2-x0(2),q1-x0(1));
-%     param=[x(1,1);x(2,1);q1;q2;v];
+%     param=[x0(1);x0(2);param(1:3)]; %Switch for point tracking
+    q1=param(1);q2=param(2);v=param(3);ve=param(4);
+    psid=atan2(q2-x0(2),q1-x0(1));
+    param=[x(1,1);x(2,1);q1;q2;v];
     for i=1:length(t)-1
-%         xe=q1+ve*cos(psid)*t(i);
-%         ye=q2+ve*sin(psid)*t(i);
-%         if mod(i,20)==0
-%            param=[x(1,i);x(2,i);xe;ye;v];
-%         end
+        xe(i)=q1+ve*cos(psid)*t(i);
+        ye(i)=q2+ve*sin(psid)*t(i);
+        if mod(i,50)==0
+           param=[x(1,i);x(2,i);xe(i);ye(i);v];
+        end
+        if (x(1,i)-xe(i))^2+(x(2,i)-ye(i))^2<1
+            return
+        end
         k1=h*f(t(i),x(:,i),param);
         k2=h*f(t(i)+h/2,x(:,i)+k1/2,param);
         k3=h*f(t(i)+h/2,x(:,i)+k2/2,param);
